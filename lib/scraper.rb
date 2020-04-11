d97cc84bbdb306f0069434bcf8c47691823bebd0
matthew enbar <matthew.enbar@gmail.com>
2020-04-11 18:22:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,17 @@ require_relative './course.rb'
 
 class Scraper
 
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
@@ -21,4 +32,5 @@ class Scraper
      course.schedule = post.css(".date").text
      course.description = post.css("p").text
    end
+ end
  end
