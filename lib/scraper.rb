69049943393366e91ef743f8f1ea6a72c51eb845
matthew enbar <matthew.enbar@gmail.com>
2020-04-11 18:23:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,11 +18,6 @@ class Scraper
   end
 
   def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end
-
-
-  def get_page
 
    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
